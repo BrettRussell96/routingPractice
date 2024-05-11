1d8150117d1a47fee8339081730dd0aea49a6a8e
Brett-Russell96 <brett.russell2016@gmail.com>
2024-05-11 11:53:31 +1000

    feat: redirect button for about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,13 @@
-
+import { useNavigate } from "react-router-dom"
 
 export default function AboutPage(){
+
+    const navigate = useNavigate();
+
     return(
-        <h1>
-            About
-        </h1>
-    )
+        <>
+        <h1>About</h1>
+        <button onClick={() => navigate("/")}>Go home</button>
+        </>
+    );
 }
