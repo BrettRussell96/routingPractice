a21c48b31fb26a40d20bf7fb6d385016f1300f65
Brett-Russell96 <brett.russell2016@gmail.com>
2024-05-11 11:07:44 +1000

    patch: import createBrowserRouter

--- src/App_ObjectRouting.jsx
+++ src/App_ObjectRouting.jsx
@@ -1,5 +1,5 @@
 
-import { RouterProvider } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css'
 import HomePage from "./pages/Home";
 import AboutPage from "./pages/About";
