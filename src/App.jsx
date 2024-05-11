c368085cd6929f8702cb7465818c30f5012a4e74
Brett-Russell96 <brett.russell2016@gmail.com>
2024-05-11 10:20:33 +1000

    feat: added pages, contact and home

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import HomePage from "./pages/Home";
+import AboutPage from "./pages/About";
+import ContactPage from "./pages/Contact";
+
+function App() {
+
+
+  return (
+     <>
+      <HomePage />
+      <AboutPage />
+      <ContactPage />
+      </> 
+  )
+}
+
+export default App
